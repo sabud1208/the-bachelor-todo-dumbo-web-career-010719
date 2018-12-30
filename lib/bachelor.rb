3049084eed141cd3912682abdcd077ddc771c69b
Stephanie Messad <sabud1208@gmail.com>
2018-12-30 06:27:06 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,22 +1,27 @@
 require "pry"
 
 def get_first_name_of_season_winner(data, season)
-  winner = []
-  data.each do |season, info|
-    info.each do |contestant|
-       contestant["status"]
-      if contestant["status"] == "Winner"
-        contestant["name"].split.first
-        winner << contestant["name"].split.first
-      end
-    end
+  data[season].each do |info|
+    binding.pry
   end
-  data.each do |season, info|
-
-
-binding.pry
 end
-end
+
+#   winner = []
+#   data.each do |season, info|
+#     info.each do |contestant|
+#        contestant["status"]
+#       if contestant["status"] == "Winner"
+#         contestant["name"].split.first
+#         winner << contestant["name"].split.first
+#       end
+#     end
+#   end
+#   data.each do |season, info|
+#     data == seaso
+#
+#
+# binding.pry
+# end
 
 def get_contestant_name(data, occupation)
   # code here
