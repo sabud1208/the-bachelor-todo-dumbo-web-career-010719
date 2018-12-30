fedb3a8efcd73481ae2579f48c892f1d6c8b0a40
Stephanie Messad <sabud1208@gmail.com>
2018-12-30 06:33:28 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,15 @@
 require "pry"
 
 def get_first_name_of_season_winner(data, season)
-  winner = " "
     data.each do |season, info|
       info.each do |contestant|
          contestant["status"]
         if contestant["status"] == "Winner"
           contestant["name"].split.first
-          winner << contestant["name"].split.first
+        contestant["name"].split.first
         end
       end
   end
-  winner
 end
 
 
