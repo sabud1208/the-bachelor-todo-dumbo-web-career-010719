c0a1e2070879be5a69c4cb5c85806f6bd8c5335a
Stephanie Messad <sabud1208@gmail.com>
2018-12-30 07:13:09 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,7 +31,7 @@ def get_contestant_name(data, occupation)
     info.each do |stats|
       stats.each  do |key, value|
         if value == occupation
-          name = stats["name"]
+          return stats["name"]
        end
       end
     end
