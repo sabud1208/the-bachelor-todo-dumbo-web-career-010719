12dc5e432bc35bfd2dd14332178f8a888c60f1c1
Stephanie Messad <sabud1208@gmail.com>
2018-12-30 05:44:28 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ require "pry"
 def get_first_name_of_season_winner(data, season)
   data.each do |season, info|
     if season == data
-      status
+      :status
     binding.pry
     #  data << info[:status]
     #
