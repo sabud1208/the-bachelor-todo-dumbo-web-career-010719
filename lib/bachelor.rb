dda0e333c1f58d53de1243d24e7074a13cb907d8
Stephanie Messad <sabud1208@gmail.com>
2018-12-30 07:43:23 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,7 +56,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |info|
     info.each do |key, value|
     if value == info["age"]
-      info["age"] += counter
+      info["age"] += counter \ info.length
       binding.pry
     end
     end
