f4bca233db791af79797380010f0d6ffb6c3cb47
Stephanie Messad <sabud1208@gmail.com>
2018-12-30 06:04:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,11 +6,11 @@ def get_first_name_of_season_winner(data, season)
        contestant["status"]
       if contestant["status"] == "Winner"
         winner = contestant["name"].split.first
-    binding.pry
       end
     end
   end
   winner
+    binding.pry
 end
 
 def get_contestant_name(data, occupation)
