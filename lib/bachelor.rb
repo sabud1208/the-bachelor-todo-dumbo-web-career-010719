4fa26ccff148c888d3637f65457f997b13daa063
Stephanie Messad <sabud1208@gmail.com>
2018-12-30 06:50:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,7 +6,7 @@ def get_first_name_of_season_winner(data, season)
       info.each do |contestant|
          contestant["status"]
         if contestant["status"] == "Winner"
-        winner << contestant["name"].split.first
+        winner << contestant["name"].split.first[0]
         end
       end
   end
